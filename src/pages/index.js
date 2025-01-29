a94940f9020aa31fb450b06fca7947765cfabce4
Trincell Shelvin <trincellshelvin@gmail.com>
2025-01-29 17:45:08 -0600

    implemented loader component with styling into site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import  { HeroSectionWithButton } from './components/HeroSection';
-
-
-const CallToActionSection = ({ handleClick }) => (
-  <section className="cta-button relative flex items-center justify-center min-h-screen">
-    <video src="/fallingcoffeebeans.mp4" loop autoPlay muted className="absolute w-full h-full object-cover top-0 left-0"></video>
-    
-  </section>
-);
+import { HeroSectionWithButton } from './components/HeroSection';
+import Loading from './components/Loader'; // Ensure correct path to Loader
 
 export default function Home() {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleCtaClick() {
     console.log('CTA button clicked!');
-    router.push('/signup');
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      router.push('/signup');
+    }, 3000); // Simulate loading for 3 seconds
   }
 
   return (
     <div data-theme="coffeeShop" className="text-chocolate-brown min-h-screen flex flex-col justify-between">
-      <Navbar />
-      <HeroSectionWithButton 
-      buttonLabel={"Signup Now"} handleCtaClick={handleCtaClick} />
-      {/* <CallToActionSection handleClick={handleCtaClick} /> */}
-      <footer className="footer relative z-10 bg-white text-chocolate-brown text-center py-4">
-        <Footer />
-      </footer>
+      <Loading isLoading={isLoading} />
+      {!isLoading && (
+        <>
+          <Navbar />
+          <HeroSectionWithButton buttonLabel={"Signup Now"} handleCtaClick={handleCtaClick} />
+          <footer className="footer relative z-10 bg-white text-chocolate-brown text-center py-4">
+            <Footer />
+          </footer>
+        </>
+      )}
+      <button onClick={handleCtaClick} className="m-4 p-2"></button>
     </div>
   );
 }
