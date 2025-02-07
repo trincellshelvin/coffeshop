09f813ce4b59ed4a2d836df218ceb410c199e0ce
Trincell Shelvin <trincellshelvin@gmail.com>
2025-02-06 18:03:10 -0600

    updated approutes for pages

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,14 +3,20 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/index';
 import LoginPage from '../pages/login';
+import SignupPage from '../pages/signup';
+import CartPage from '../pages/cart';
+import CheckoutPage from '../pages/checkout';
 import DashboardPage from '../pages/DashboardPage';
-import ProtectedRoute from '../pages/components/ProtectedRoute'; 
+import ProtectedRoute from '../pages/components/ProtectedRoute';
 
 const AppRoutes = () => (
     <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/DashboardPage" element={
             <ProtectedRoute>
                 <DashboardPage />
             </ProtectedRoute>
