95dc62a670c273e0e0069734ab54eb5254fb26fb
Trincell Shelvin <trincellshelvin@gmail.com>
2025-02-12 21:25:00 -0600

    removed routes page approutes.js to see if this will now resolve the dom issue with vercel

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,17 +1,17 @@
-// src/AppRoutes.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ProtectedRoute from '../components/ProtectedRoute';
-import HomePage from '../pages/index';
-import LoginPage from '../pages/login';
-import DashboardPage from '../pages/DashboardPage';
+// // src/AppRoutes.js
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import ProtectedRoute from '../components/ProtectedRoute';
+// import HomePage from '../pages/index';
+// import LoginPage from '../pages/login';
+// import DashboardPage from '../pages/DashboardPage';
 
-const AppRoutes = () => (
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <ProtectedRoute path="/dashboard" component={DashboardPage} />
-    </Routes>
-);
+// const AppRoutes = () => (
+//     <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <ProtectedRoute path="/dashboard" component={DashboardPage} />
+//     </Routes>
+// );
 
-export default AppRoutes;
+// export default AppRoutes;
